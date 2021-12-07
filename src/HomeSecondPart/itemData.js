2b730c2cfbd3faa21603bc27be60d0b2bc44c0fa
Shootingvision <shootingvision@gmail.com>
2021-12-07 18:37:32 +0100

    homepage wip

---
+++ src/HomeSecondPart/itemData.js
@@ -0,0 +1,41 @@
+const itemData = [
+    {
+      img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+      title: 'Breakfast',
+      rows: 1,
+      cols: 1,
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+      title: 'Burger',
+      rows: 1,
+      cols: 1,
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+      title: 'Camera',
+      rows: 2,
+      cols: 2,
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+      title: 'Coffee',
+      rows: 1,
+      cols: 1,
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+      title: 'Hats',
+      rows: 1,
+      cols: 1,
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+      title: 'Honey',
+      rows: 2,
+      cols: 2,
+    },
+
+  ];
+
+  export default itemData;
